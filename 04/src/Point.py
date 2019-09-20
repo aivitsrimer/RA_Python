7c438cffc806947a8f85a879440622505828cfa7
ksk- <sssss@ffg,io>
2019-09-20 19:52:10 +0700

    Added class Point

--- 04/src/Point.py
+++ 04/src/Point.py
@@ -1,31 +1,33 @@
 from math import *
 
-from dataclasses import dataclass
 
-
-@dataclass
 class Point:
-    x: float
-    y: float
+    def __init__(self, x=0.0, y=0.0, z=0.0):
+        self.x: float = x
+        self.y: float = y
+        self.z: float = z
+
+    def to_vector(self) -> tuple:
+        return self.x, self.y, self.z
+
+    def to_json(self) -> str:
+        import json
+        return json.dumps({
+            'x': self.x,
+            'y': self.y,
+            'z': self.z,
+        })
+
+    def distance_to(self, other) -> float:
+        return sqrt(
+            (other.x - self.x) ** 2 +
+            ((other.y - self.y) ** 2) +
+            ((other.z - self.z) ** 2)
+        )
 
 
-def to_vector(a: Point) -> tuple:
-    return a.x, a.y
-
-
-def to_json(a: Point) -> str:
-    import json
-    return json.dumps({
-        'x': a.x,
-        'y': a.y,
-    })
-
-
-def distance(a: Point, b: Point) -> float:
-    return sqrt((b.x - a.x) ** 2 + ((b.y - a.y) ** 2))
-
-
-p = Point(3, 4)
-print(distance(p, Point(-98.8, 7)))
-print(to_vector(p))
-print(to_json(p))
+p = Point(x=0.9, y=5, z=0.0)
+print(p.distance_to(Point(-98.8, 7, 0)))
+print(p.to_vector())
+print(p.to_vector)
+print(p.to_json())
